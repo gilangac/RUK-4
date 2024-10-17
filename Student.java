ff164db4cc5913d15a0db71c6b5c1e865002025c
DimasFatchurroziq <dimasfatchurroziq@gmail.com>
2024-10-17 18:18:45 +0700

    push

--- Student.java
+++ Student.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 class Student extends Person {
+    public String studentName;
+    private int studentAge;
     private String major;
 
     public Student() {
-        super();
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama: ");
+        this.studentName = scanner.nextLine();
+        System.out.print("Masukkan umur: ");
+        this.studentAge = scanner.nextInt();
+        scanner.nextLine();
+        // super();
+        Scanner scanner2 = new Scanner(System.in);
         System.out.print("Masukkan jurusan: ");
-        this.major = scanner.nextLine();
+        this.major = scanner2.nextLine();
     }
 
     @Override
     public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Major: " + major);
+        System.out.println("Name: " + studentName + ", Age: " + studentAge + ", Major" + major);
+        // super.displayInfo();
+        // System.out.println("Major: " + major);
     }
 }
--- Teacher.java
+++ Teacher.java
@@ -1,18 +1,27 @@
 import java.util.Scanner;
 
 class Teacher extends Person {
+    public String teacherName;
+    private int teacherAge;
     private String subject;
 
     public Teacher() {
-        super();
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan nama: ");
+        this.teacherName = scanner.nextLine();
+        System.out.print("Masukkan umur: ");
+        this.teacherAge = scanner.nextInt();
+        scanner.nextLine();
+        // super();
+        Scanner scanner2 = new Scanner(System.in);
         System.out.print("Masukkan mata pelajaran: ");
-        this.subject = scanner.nextLine();
+        this.subject = scanner2.nextLine();
     }
 
     @Override
     public void displayInfo() {
-        super.displayInfo();
-        System.out.println("Subject: " + subject);
+        System.out.println("Name: " + teacherName + ", Age: " + teacherAge + ", Subject" + subject);
+        // super.displayInfo();
+        // System.out.println("Subject: " + subject);
     }
 }
